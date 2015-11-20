ad845c3ed5385ce0516b132bebb420b8085ffe7f
jawittdesigns <Killjoy11>
2015-11-20 10:48:51 -0500

    Removed JS from footer

--- footer.php
+++ footer.php
@@ -65,48 +65,7 @@ $bene_terms_label = ( get_field( 'bene_terms_label', 'option' ) ? get_field( 'be
     
    </div>
 </footer>
-
 <?php wp_footer(); ?>
 </div>
-
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-32824835-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
-
-<!-- CLARIVOY CODE / DO NOT REMOVE -->
-
-<script type="text/javascript">
-var ut_params = ut_params || [];ut_params.push("UT-736573600");//version:0.4
-(function() {var ut = document.createElement('script'); ut.type = 'text/javascript'; ut.async = true;
-ut.src = (("https:" == document.location.protocol) ? "https://" : "http://") + 'pixel.claritytag.com/javascripts/clarity.js';
-var script = document.getElementsByTagName('script')[0]; script.parentNode.insertBefore(ut, script);})();
-</script>
-
-<!-- END CLARIVOY CODE -->
-
-<!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//analytics.fcdev.net/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 3]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<noscript><p><img src="//analytics.fcdev.net/piwik.php?idsite=3" style="border:0;" alt="" /></p></noscript>
-<!-- End Piwik Code -->
-
-
 </body>
 </html>
